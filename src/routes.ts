c158bb2735952220a229bfd372f2617dac0a3079
Simone P <simo.picci94@gmail.com>
2022-09-14 13:19:21 +0200

    refactor: changed config file to function

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import type { Type } from '@angular/core';
 import type { Route } from '@angular/router';
 
-type Module = { default: Type<any> };
-type RouteConfig = { default: Route };
+type RouteConfigFunc = { (): Route };
 
-const CONFIGS = import.meta.glob<RouteConfig>(['./pages/**/*.config.ts'], {
-  eager: true,
-});
+type Module = {
+  default: Type<any>;
+  getRouteConfig: RouteConfigFunc | undefined;
+};
 
 const FILES = import.meta.glob<Module>([
   './pages/**/*.ts',
@@ -15,80 +15,84 @@ const FILES = import.meta.glob<Module>([
 
 const ROUTES = Object.keys(FILES).sort((a, b) => a.length - b.length);
 
-const routeConfigs = ROUTES.reduce<Route[]>((routes: Route[], key: string) => {
-  const module = FILES[key];
-  const configKey = key.replace(/\.ts$/, '.config.ts');
-  const config = CONFIGS[configKey] || {};
-  const routeConfig = config?.default;
+const routeConfigs = await ROUTES.reduce<Promise<Route[]>>(
+  async (_routes: Promise<Route[]>, key: string) => {
+    const routes = await _routes;
+    const module = await FILES[key]();
+    const routeConfig = module.getRouteConfig
+      ? module.getRouteConfig() || {}
+      : {};
 
-  const route: Route = {
-    loadComponent: () => module().then((mod) => mod.default),
-    ...routeConfig,
-  };
+    const route: Route = {
+      loadComponent: () => module.default,
+      ...routeConfig,
+    };
 
-  const segments = key
-    .replace(/\.\/pages|\.(js|ts)$/g, '')
-    .replace(/\[\.{3}.+\]/, '**')
-    .replace(/\[([^\]]+)\]/g, ':$1')
-    .split('/')
-    .filter(Boolean);
+    const segments = key
+      .replace(/\.\/pages|\.(js|ts)$/g, '')
+      .replace(/\[\.{3}.+\]/, '**')
+      .replace(/\[([^\]]+)\]/g, ':$1')
+      .split('/')
+      .filter(Boolean);
 
-  segments.reduce((parent, segment, index) => {
-    const path = segment.replace(/index|\./g, '');
-    const isIndex = !path;
-    const isCatchall = path === '**';
-    const pathMatch = isIndex ? 'full' : undefined;
-    const root = index === 0;
-    const leaf = index === segments.length - 1 && segments.length > 1;
-    const node = !root && !leaf;
-    const insert = /^\w|\//.test(path) && !isCatchall ? 'unshift' : 'push';
+    segments.reduce((parent, segment, index) => {
+      const path = segment.replace(/index|\./g, '');
+      const isIndex = !path;
+      const isCatchall = path === '**';
+      const pathMatch = isIndex ? 'full' : undefined;
+      const root = index === 0;
+      const leaf = index === segments.length - 1 && segments.length > 1;
+      const node = !root && !leaf;
+      const insert = /^\w|\//.test(path) && !isCatchall ? 'unshift' : 'push';
 
-    if (root) {
-      const dynamic = path.startsWith(':');
-      if (dynamic) return parent;
+      if (root) {
+        const dynamic = path.startsWith(':');
+        if (dynamic) return parent;
 
-      const last = segments.length === 1;
-      if (last) {
-        const newRoute = { path, pathMatch, ...route };
+        const last = segments.length === 1;
+        if (last) {
+          const newRoute = { path, pathMatch, ...route };
 
-        routes?.[insert](newRoute as Route);
+          routes?.[insert](newRoute as Route);
 
-        return parent;
+          return parent;
+        }
       }
-    }
 
-    if (root || node) {
-      const current = root ? routes : parent.children;
-      const found = current?.find((route) => route.path === path);
+      if (root || node) {
+        const current = root ? routes : parent.children;
+        const found = current?.find((route) => route.path === path);
 
-      if (found) {
-        found.children ??= [];
-        found.pathMatch = pathMatch;
-      } else
-        current?.[insert]({
+        if (found) {
+          found.children ??= [];
+          found.pathMatch = pathMatch;
+        } else
+          current?.[insert]({
+            path,
+            pathMatch,
+            children: [],
+          });
+
+        return (
+          found ||
+          (current?.[insert === 'unshift' ? 0 : current.length - 1] as Route)
+        );
+      }
+
+      if (leaf) {
+        parent?.children?.[insert]({
           path,
           pathMatch,
-          children: [],
+          ...route,
         });
+      }
 
-      return (
-        found ||
-        (current?.[insert === 'unshift' ? 0 : current.length - 1] as Route)
-      );
-    }
+      return parent;
+    }, {} as Route);
 
-    if (leaf) {
-      parent?.children?.[insert]({
-        path,
-        pathMatch,
-        ...route,
-      });
-    }
-
-    return parent;
-  }, {} as Route);
-
-  return routes;
-}, []);
+    return routes;
+  },
+  Promise.resolve([])
+);
 
 export const routes: Route[] = [...routeConfigs];
